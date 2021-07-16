7712c77ccf767c111c85a22a1eeae6532bdc5b82
ZiYooh <natsucomi42@gmail.com>
2021-07-16 16:09:48 +0900

    함수 추가

--- app/src/index.js
+++ app/src/index.js
@@ -32,28 +32,57 @@ const App = {
         }
     },
 
-    populateCandidates: async function () {
-        await this.loadCandidates();
-        this.setupCandidateRows();
+    populateCandidates: async function() {
+      await this.loadCandidates();
+      this.setupCandidateRows();
+      await this.populateTokenData();
+    
+      const { totalVotesFor } = this.voting.methods;
+      let candidateNames = Object.keys(candidates);
+      for (var i = 0; i < candidateNames.length; i++) {
+        let name = candidateNames[i];
+        var count = await totalVotesFor(this.web3.utils.asciiToHex(name)).call();
+        $("#" + candidates[name]).html(count);
+      }
+    },
+    
+    loadCandidates: async function() {
+      const { allCandidates } = this.voting.methods;
+      
+      let candidateArray = await allCandidates().call();
+      for(let i=0; i < candidateArray.length; i++) {
+        /* We store the candidate names as bytes32 on the blockchain. We use the
+        * handy toUtf8 method to convert from bytes32 to string
+        */
+        candidates[this.web3.utils.hexToUtf8(candidateArray[i])] = "candidate-" + i;
+      }
+    },
+    
+    setupCandidateRows: function() {
+      Object.keys(candidates).forEach(function (candidate) { 
+        $("#candidate-rows").append("" + candidate + "");
+      });
+    },
+    
+    populateTokenData: async function() {
+      const { web3 } = this;
+      const { totalTokens, tokensSold, tokenPrice } = this.voting.methods;
+    
+      let value = await totalTokens().call();
+      $("#tokens-total").html(value.toString());
+    
+      value = await tokensSold().call();
+      $("#tokens-sold").html(value.toString());
+    
+      value = await tokenPrice().call();
+      pricePerToken = web3.utils.fromWei(value.toString());
+      $("#token-cost").html(pricePerToken + " Ether");
+    
+      web3.eth.getBalance(this.contractAddress, function(error, result) {
+        $("#contract-balance").html(web3.utils.fromWei(result.toString()) + " Ether");
+      });
     },
 
-    loadCandidates: async function () {
-        const {allCandidates} = this.voting.methods;
-
-        let candidateArray = await allCandidates().call();
-        for (let i = 0; i < candidateArray.length; i++) {
-            /* We store the candidate names as bytes32 on the blockchain. We use the
-    * handy toUtf8 method to convert from bytes32 to string
-    */
-            candidates[this.web3.utils.hexToUtf8(candidateArray[i])] = "candidate-" + i;
-        }
-    },
-
-    setupCandidateRows: function () {
-        Object.keys(candidates).forEach(function (candidate) {
-          $("#candidate-rows").append("" + candidate + "");
-        });
-    }
 };
 
 window.App = App;
@@ -72,6 +101,5 @@ window.addEventListener("load", function () {
         // mgmt / fail)
         App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"),);
     }
-
     App.start();
 });
